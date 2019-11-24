adf3c1e4486745ac34a97d81edae0470f8bb4227
Allison Patton <allison.tpttn@gmail.com>
2019-11-24 15:32:04 -0600

    Added Identity Framework to existing Entity Framework Code-along practice and now I have register/login working

---
+++ IdentityIntro/Models/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityIntro.Models
+{
+    //already has an Id
+    public class ApplicationUser : IdentityUser
+    {
+        public string SlackHandle { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public virtual ICollection<Animal> Animals { get; set; }
+    }
+}
